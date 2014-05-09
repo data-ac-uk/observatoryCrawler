7938bfdab012a26a6716ec2755b36a899f72cdf5
Andrew Milsted <a.j.milsted@soton.ac.uk>
2014-05-09 14:58:09 +0100

    made a config file

---
+++ etc/config.php
@@ -0,0 +1,23 @@
+<?php
+	
+	$config = (object) NULL;
+	$config->pwd = dirname(__DIR__);
+
+	$config->db = (object) NULL;
+	$config->db->connection = "mysql:host=localhost;port=3306;dbname=crawler";
+	$config->db->user = 'crawler';
+	$config->db->password = 'crawler';
+
+	$config->date = (object) NULL;
+	$config->date->mysql = 'Y-m-d H:i:s';
+	$config->date->mysqldate = 'Y-m-d';
+
+	$config->crawl = (object) NULL;
+	$config->crawl->cache = 24; //in Hours
+	$config->crawl->timeout = 10;
+	$config->crawl->agent = 'AcUkCrawler';
+	$config->crawl->locationlimit = 10;
+	$config->crawl->emailTo = array("andy@data.ac.uk");
+
+
+	$this->config = $config;
--- uk.ac.data.Crawler.php
+++ uk.ac.data.Crawler.php
@@ -8,27 +8,8 @@ class ukacdataCrawler {
 	public $config = array();
 	
 	function __construct(){
-		$config = (object) NULL;
-		$config->pwd = __DIR__;
 		
-		$config->db = (object) NULL;
-		$config->db->connection = "mysql:host=localhost;port=3306;dbname=crawler";
-		$config->db->user = 'crawler';
-		$config->db->password = 'crawler';
-		
-		$config->date = (object) NULL;
-		$config->date->mysql = 'Y-m-d H:i:s';
-		$config->date->mysqldate = 'Y-m-d';
-		
-		$config->crawl = (object) NULL;
-		$config->crawl->cache = 24; //in Hours
-		$config->crawl->timeout = 10;
-		$config->crawl->agent = 'AcUkCrawler';
-		$config->crawl->locationlimit = 10;
-		$config->crawl->emailTo = array("andy@data.ac.uk");
-	
-	
-		$this->config = $config;
+		require(__DIR__."/etc/config.php");
 		
 		$this->launch_db();
 	}
